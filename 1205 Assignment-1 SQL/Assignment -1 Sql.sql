e4c66fed9b5ebe3f47d5182fef1641cc9ae8ad52
Harshvardhan Gour <136323519+HarshvardhanGour@users.noreply.github.com>
2024-01-11 17:12:21 +0530

    Add files via upload

--- 1205 Assignment-1 SQL/Assignment -1 Sql.sql
+++ 1205 Assignment-1 SQL/Assignment -1 Sql.sql
@@ -21,6 +21,30 @@ insert into ORG values(108, "riyaverma", "riya02@gmail.com", "2009-12-01");
 insert into ORG values(109, "Saloni verma", "Saloniverma909@gmail.com", "2018-01-20");
 insert into ORG values(110, "Tanmay gour", "Tanmaygour2@gmail.com", "2010-04-25");
 
+create database orders;
+
+use orders;
+CREATE TABLE orders
+(
+Order_ID INT (23),
+Customer_1D int (23) ,
+OrderDate Date,
+TotalAmount decimal
+);
+select * from orders;
+
+insert into orders values(101, "0201", "2022-02-02", "4000");
+insert into orders values(102, "0202", "2019-08-10", "500");
+insert into orders values(103, "0203", "2021-03-23", "5500");
+insert into orders values(104, "0204", "2022-04-25", "6000");
+insert into orders values(105, "0205", "2022-02-09", "3000");
+insert into orders values(106, "0206", "2022-03-12", "2500");
+insert into orders values(107, "0207", "2022-11-23", "4200");
+insert into orders values(108, "0208", "2022-01-29", "4120");
+insert into orders values(109, "0209", "2022-02-04", "4300");
+insert into orders values(110, "0210", "2022-03-05", "3500");
+
+
 
 
 create database orderdetails;
@@ -47,4 +71,28 @@ insert into orderdetails values(129,"07","08","65","2400");
 insert into orderdetails values(110,"08","09","77","1900");
 insert into orderdetails values(111,"09","10","33","3000");
 insert into orderdetails values(112,"10","11","09","5000");
+
+create database Products;
+
+use Products;
+CREATE TABLE Products
+(
+Product_ID INT (23),
+Name_full varchar (23),
+Category varchar (50),
+Price decimal (10,2)
+);
+
+select * from Products;
+
+insert into Products values(101,"Iphone","Smartphones","1.50000");
+insert into Products values(102,"samsung","Flip Phones","1.2900");
+insert into Products values(103," galaxy Z filp","Flip Phones","1.3900");
+insert into Products values(104,"Nokia 6700","Slider Phones","1500");
+insert into Products values(105,"BlackBerry","QWERTY Phones","12900");
+insert into Products values(106,"Sonim Xp5","Rugged Phones","10000");
+insert into Products values(107,"samsung galaxy Note20","Phablets","13500");
+insert into Products values(108," Oneplus","Foldable Phones","12000");
+insert into Products values(109,"Nokia","Slider Phones","45000");
+insert into Products values(110,"BlackBerry 354","QWERTY Phones","12900");
 
